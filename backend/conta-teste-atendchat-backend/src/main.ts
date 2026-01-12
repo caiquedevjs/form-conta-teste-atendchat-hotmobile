82e2155e44a522fddc08863b2810e3228d7894b5
caiquedevjs <caiquemenezes1@outlook.com>
2026-01-12 16:33:40 -0300

    att

--- backend/conta-teste-atendchat-backend/src/main.ts
+++ backend/conta-teste-atendchat-backend/src/main.ts
@@ -9,10 +9,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   // 2. ATIVANDO O CORS AQUI
-app.enableCors({
-    origin: '*', 
+// 2. CORREÇÃO DO CORS
+  app.enableCors({
+    // Copie EXATAMENTE como aparece no navegador (sem a barra / no final)
+    origin: 'https://form-conta-teste-atendchat-hotmobil.vercel.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    
+    credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
