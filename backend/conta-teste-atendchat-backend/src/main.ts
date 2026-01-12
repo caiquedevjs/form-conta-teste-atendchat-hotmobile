35fe4edb23d49b99834d7c3e87bca1a03f86b248
caiquedevjs <caiquemenezes1@outlook.com>
2026-01-12 16:15:39 -0300

    att

--- backend/conta-teste-atendchat-backend/src/main.ts
+++ backend/conta-teste-atendchat-backend/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
